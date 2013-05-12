193fe572567f8091d80dcda9cd775c38518c8cc4
Darin Creason <darin.creason@gmail.com>
2013-05-12 11:03:46 -0700

    First commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.ReadAllLines(args[0]);
+        }
+    }
+}
+
+
+public class given_an_input_pipe
+{
+    protected readonly InputPipe sut;
+
+    [Fact]
+    public void when_reading_from_well_formed_file_then_enumerate()
+    {
+        var reader = new StringReader("2,30");
+        reader.ReadLineAsync();
+        foreach (var item in InputPipe.FromReader(reader)) ;
+    }
+}
+
